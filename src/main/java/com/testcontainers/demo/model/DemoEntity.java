722e373479377b1993ec38202d64f81aae2970a1
Igor Kostiakov <igkostiakov@gmail.com>
2022-01-07 13:02:50 +0300

    Add liquibase

--- src/main/java/com/testcontainers/demo/model/DemoEntity.java
+++ src/main/java/com/testcontainers/demo/model/DemoEntity.java
@@ -5,10 +5,12 @@ import lombok.Data;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import java.util.UUID;
 
 @Data
 @Entity
+@Table(name = "demo")
 public class DemoEntity {
 
     @Id
